159156c48ca5ec2ea6c747c0f36689047878dccc
Nithyananda R S <123256904+Nithyananda2003@users.noreply.github.com>
2025-09-03 21:26:51 -0400

    Update browserLaunch.js

--- utils/chromium/browserLaunch.js
+++ utils/chromium/browserLaunch.js
@@ -1,31 +1,18 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
+import { executablePath } from 'puppeteer'; // works in puppeteer v21+
 
 const getBrowserInstance = async () => {
-    try {
-        const browser = await puppeteer.launch({
-            headless: true, // or "new" for newer Puppeteer
-            args: [
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                '--disable-dev-shm-usage',
-                '--disable-accelerated-2d-canvas',
-                '--no-zygote',
-                '--disable-gpu'
-            ],
-            ignoreHTTPSErrors: true,
-            timeout: 60000,          // give Chrome more time to boot
-            protocolTimeout: 60000
-        });
-
-        browser.on('disconnected', () => {
-            console.log('Browser disconnected');
-        });
-
-        return browser;
-    } catch (error) {
-        console.error('Error launching browser:', error);
-        throw error;
-    }
+    const browser = await puppeteer.launch({
+        headless: true,
+        executablePath: executablePath(), // system Chrome on Render
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu'
+        ]
+    });
+    return browser;
 };
 
 export default getBrowserInstance;
